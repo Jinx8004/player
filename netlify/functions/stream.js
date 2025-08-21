4106ccb49c5b1397cfef820951cd7275d5334bc4
Anmol <anmolpatel2373@gmail.com>
2025-08-21 19:16:15 +0530

    Test: Use public stream

--- netlify/functions/stream.js
+++ netlify/functions/stream.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  // Get the original stream URL from the query parameter
   const streamUrl = event.queryStringParameters.url;
 
   if (!streamUrl) {
@@ -12,23 +11,34 @@ exports.handler = async function (event, context) {
   }
 
   try {
-    // Fetch the M3U8 content from the real server
-    // NEW: We now add a 'Referer' header as well
+    // We are now sending a full suite of headers to mimic a real browser request
     const response = await fetch(streamUrl, {
       headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Sec-Ch-Ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-        'Referer': 'https://dai.google.com/'
+        'Referer': 'https://www.google.com/' // Using a generic Referer
       }
     });
 
     const data = await response.text();
 
-    // Return the content back to our video player
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/vnd.apple.mpegurl',
-        'Access-Control-Allow-Origin': '*', // Allow our site to access this
+        'Access-Control-Allow-Origin': '*',
       },
       body: data,
     };
